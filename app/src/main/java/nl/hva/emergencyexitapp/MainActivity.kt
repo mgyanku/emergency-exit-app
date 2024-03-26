eee4a1706190a874a1d1664d27aa65e15034e108
Miriam Yanku <miriam.yanku@hva.nl>
2024-03-26 14:52:23 +0100

    app bottom bar implemented

--- app/src/main/java/nl/hva/emergencyexitapp/MainActivity.kt
+++ app/src/main/java/nl/hva/emergencyexitapp/MainActivity.kt
@@ -4,19 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import nl.hva.emergencyexitapp.ui.theme.EmergencyExitAppTheme
+import nl.hva.emergencyexitapp.ui.theme.coralPink
 import nl.hva.emergencyexitapp.ui.theme.screens.AppScreens
 import nl.hva.emergencyexitapp.ui.theme.screens.HomeScreen
+import nl.hva.emergencyexitapp.ui.theme.white
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,43 +45,92 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    // Call base scaffold
+                    AppScaffold()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+private fun AppScaffold() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        color = white
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = coralPink)
+            )
+        },
+        bottomBar = {
+            BottomNav(navController)
+        },
+        content = { innerPadding ->
+            AppNavHost(
+                navController, modifier = Modifier.padding(innerPadding)
+            )
+        },
     )
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EmergencyExitAppTheme {
-        Greeting("Android")
-    }
-}
+
 
 /**
  * You can see this as a nav_graph.xml in compose environment.
  */
 @Composable
-private fun RemindersNavHost(
+private fun AppNavHost(
     navController: NavHostController, modifier: Modifier
 ) {
     NavHost(
         navController = navController,
-        startDestination = AppScreens.HomeScreen.name,
+        startDestination = AppScreens.HomeScreen.route,
         modifier = modifier
     ) {
-        composable(route = AppScreens.HomeScreen.name) {
+        composable(route = AppScreens.HomeScreen.route) {
             HomeScreen(navController)
+        }
+    }
+}
+
+@Composable
+fun BottomNav(navController: NavHostController) {
+    val items = listOf(
+        AppScreens.HomeScreen,
+        AppScreens.SearchScreen,
+    )
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        items.forEach { screen ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = screen.icon),
+                        contentDescription = null
+                    )
+                },
+                label = { Text(stringResource(screen.resourceId)) },
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
         }
     }
 }
--- app/src/main/java/nl/hva/emergencyexitapp/ui/theme/screens/AppScreens.kt
+++ app/src/main/java/nl/hva/emergencyexitapp/ui/theme/screens/AppScreens.kt
@@ -1,7 +1,14 @@
 package nl.hva.emergencyexitapp.ui.theme.screens
 
-enum class AppScreens {
-    HomeScreen,
-    SearchScreen,
-    SettingsScreen
+import androidx.annotation.StringRes
+import nl.hva.emergencyexitapp.R
+
+sealed class AppScreens (
+    val route: String,
+    @StringRes val resourceId: Int,
+    val icon: Int
+) {
+    object HomeScreen: AppScreens("home_screen", R.string.home , R.drawable.home)
+    object SearchScreen: AppScreens("search_screen", R.string.search , R.drawable.search)
+
 }
